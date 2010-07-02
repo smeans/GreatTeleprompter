7b3cc35bb567bb9ac6809c1737072a1cd8f21384
Scott Means <smeans.gid@gmail.com>
2010-07-02 13:16:58 -0400

    008-004 More dynamic layout

--- Classes/FlipsideViewController.m
+++ Classes/FlipsideViewController.m
@@ -41,14 +41,9 @@
 }
 
 
-/*
-// Override to allow orientations other than the default portrait orientation.
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-	// Return YES for supported orientations
-	return (interfaceOrientation == UIInterfaceOrientationPortrait);
+	return YES;
 }
-*/
-
 
 - (void)dealloc {
     [super dealloc];
